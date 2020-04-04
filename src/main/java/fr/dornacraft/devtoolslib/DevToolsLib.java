edd01425296c447b85c8dd2ebf6ba8695ebc0e8e
Voltariuss <loic.duboistermoz@gmail.com>
2020-04-04 22:41:31 +0200

    Initialize plugin main file

---
+++ src/main/java/fr/dornacraft/devtoolslib/DevToolsLib.java
@@ -0,0 +1,16 @@
+package fr.dornacraft.devtoolslib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DevToolsLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
